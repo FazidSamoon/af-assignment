c19c81ff2b6086e534cb3ef5a539d4be44c002ba
SachithWickramasekara <sachithwickramasekara@gmail.com>
2023-05-07 21:40:15 +0530

    Copy pasted components in education section

---
+++ client/src/pages/food/index.jsx
@@ -0,0 +1,14 @@
+import FoodSection from "@/components/organisms/FoodSectiom/FoodSection";
+import Navbar from "@/components/organisms/navbar/Navbar";
+import React from "react";
+
+const index = () => {
+  return (
+    <div className="">
+      <Navbar />
+      <FoodSection />
+    </div>
+  );
+};
+
+export default index;
